e876640e98b9b323e2ead8c39c8e0b40f4a47223
Philip Cho <Hyunsu.Cho@trincoll.edu>
2013-10-14 13:22:08 -0400

    Accumulate permutations in dlaed2
    ... so that we shuffle eigenvectors once rather than multiple times.

--- matlab/dlaed2.m
+++ matlab/dlaed2.m
@@ -14,16 +14,11 @@ z = z / sqrt(2);
 rho = abs(2 * rho);  % rho = abs(norm(z)^2 * rho)
 
 % apply perm1 to re-merge deflated eigenvalues.
-z = z(perm1);
-D = D(perm1);
-Q = Q(:, perm1);
-
 % compute perm2 that merge-sorts D1, D2 into one sorted list.
-perm2 = dlamrg(N1, N2, D, 1, 1);
+perm2 = dlamrg(N1, N2, D(perm1), 1, 1);
+
 % apply perm2.
-z = z(perm2);
-D = D(perm2);
-Q = Q(:, perm2);
+permacc = perm1(perm2);
 
 % compute allowable deflation tolerance.
 [~, imax] = max(abs(z));
@@ -33,6 +28,8 @@ tol = 8.0 * eps * max( [abs(D(jmax)), abs(z(imax))] );
 % If the rank-1 modifier is small enough, we're done: all eigenvalues deflate.
 if rho * abs(z(imax)) <= tol
     K = 0;
+    D = D(permacc);
+    Q = Q(:, permacc);
     w = zeros(0, 1);
     return;
 end
@@ -42,33 +39,36 @@ perm3 = zeros(N, 1);
 K = 0;
 K2 = N;
 for i=1:N
-    if rho * abs(z(i)) <= tol
+    pi = permacc(i);
+    if rho * abs(z(pi)) <= tol
         % 1) z-component is small
         % => move D(i) to the end of list.
         perm3(K2) = i;
         K2 = K2 - 1;
     elseif i < N
-        t = abs(D(i+1) - D(i));
-        tau = dlapy2(z(i), z(i+1));
-        s = -z(i) / tau;
-        c = z(i+1) / tau;
+        ni = permacc(i+1);
+        t = abs(D(ni) - D(pi));
+        tau = dlapy2(z(pi), z(ni));
+        s = -z(pi) / tau;
+        c = z(ni) / tau;
         if abs(t * c * s) <= tol
             % 2) D(i) and D(i+1) are close to each other compared to the
             %    z-weights given to them.
             % => zero out z(i) by applying a Givens rotation. After this step
             %    D(i) can be deflated away.
-            z(i+1) = tau;
-            z(i) = 0.0;
-            pq = Q(:, i);
-            nq = Q(:, i+1);
-            Q(:, i)   =  c * pq + s * nq;
-            Q(:, i+1) = -s * pq + c * nq;
-            t      = c^2 * D(i) + s^2 * D(i+1);
-            D(i+1) = s^2 * D(i) + c^2 * D(i+1);
-            D(i)   = t;
+            z(ni) = tau;
+            z(pi) = 0.0;
+            pq = Q(:, pi);
+            nq = Q(:, ni);
+            Q(:, pi) =  c * pq + s * nq;
+            Q(:, ni) = -s * pq + c * nq;
+            t     = c^2 * D(pi) + s^2 * D(ni);
+            D(ni) = s^2 * D(pi) + c^2 * D(ni);
+            D(pi) = t;
             perm3(K2) = i;
             k = 0;
-            while K2+k+1 <= N && D(perm3(K2+k)) < D(perm3(K2+k+1)) 
+            while K2+k+1 <= N && ...
+                D(permacc(perm3(K2+k))) < D(permacc(perm3(K2+k+1))) 
                 t = perm3(K2+k);
                 perm3(K2+k) = perm3(K2+k+1);
                 perm3(K2+k+1) = t;
@@ -88,7 +88,8 @@ for i=1:N
 end
 
 % Apply perm3 to eigenpairs.
-D = D(perm3);
-Q = Q(:, perm3);
-z = z(perm3);
+permacc = permacc(perm3);
+D = D(permacc);
+Q = Q(:, permacc);
+z = z(permacc);
 w = z(1:K);
--- matlab/test_dlaed0_random.m
+++ matlab/test_dlaed0_random.m
@@ -1,107 +1,19 @@
-% running sums of errors
-rs_max_eig_error = 0.0;
-rs_max_decomp_error = 0.0;
-rs_time = 0.0;
-
 %% symmetric tridiagonal matrices with all eigenvalues are of multiplicity 2
 %% in which the eigenvalues are clearly separated.
-%{
-diary('multiplicity_separated.log');
 v = [(1:1:500); (1:1:500)];
 v = v(:);
-for i=1:200
-    i
-    [U, ~] = qr(randn(1000));
-    A = U * diag(v) * U'; % eigenvalues 1:1:100, each with multiplicity 2
-    A = triu(A) + tril(A) - diag(diag(A)); % make it perfectly symmetric
-    A = hess(A); % reduce A to tridiagonal using orthogonal transformations
-    D = diag(A);
-    E = diag(A, 1);
-    tic;
-    [d, q] = dlaed0(D, E);
-    t = toc;
-    max_eig_error = max(abs(d - v));
-    max_decomp_error = max(max(abs(A - q * diag(d) * q')));
-    rs_max_eig_error = rs_max_eig_error + max_eig_error;
-    rs_max_decomp_error = rs_max_decomp_error + max_decomp_error;
-    rs_time = rs_time + t;
-    fprintf(1, 'Running average of max_eig_error = %.12g\n', ...
-        rs_max_eig_error / i);
-    fprintf(1, 'Running average of max_decomp_error = %.12g\n', ...
-        rs_max_decomp_error / i);
-    fprintf(1, 'Running average of running time = %.12g\n', rs_time / i);
-end
-diary off;
-%}
+%random_tester('multiplicity_separated.log', v, 200);
 
 %% symmetric tridiagonal matrices with all eigenvalues are of multiplicity 2
 %% in which the eigenvalues are not so clearly separated.
-%{
-% running sums of errors
-rs_max_eig_error = 0.0;
-rs_max_decomp_error = 0.0;
-rs_time = 0.0;
-diary('multiplicity_not_separated.log');
 v = [(1:0.0000001:1.0000499); (1:0.0000001:1.0000499)];
 v = v(:);
-for i=1:200
-    i
-    [U, ~] = qr(randn(1000));
-    A = U * diag(v) * U'; % eigenvalues 1:1:100, each with multiplicity 2
-    A = triu(A) + tril(A) - diag(diag(A)); % make it perfectly symmetric
-    A = hess(A); % reduce A to tridiagonal using orthogonal transformations
-    D = diag(A);
-    E = diag(A, 1);
-    tic;
-    [d, q] = dlaed0(D, E);
-    t = toc;
-    max_eig_error = max(abs(d - v));
-    max_decomp_error = max(max(abs(A - q * diag(d) * q')));
-    rs_max_eig_error = rs_max_eig_error + max_eig_error;
-    rs_max_decomp_error = rs_max_decomp_error + max_decomp_error;
-    rs_time = rs_time + t;
-    fprintf(1, 'Running average of max_eig_error = %.12g\n', ...
-        rs_max_eig_error / i);
-    fprintf(1, 'Running average of max_decomp_error = %.12g\n', ...
-        rs_max_decomp_error / i);
-    fprintf(1, 'Running average of running time = %.12g\n', rs_time / i);
-end
-diary off;
-%}
+%random_tester('multiplicity_not_separated.log', v, 200);
 
 %% symmetric tridiagonal matrices with two clusters, one small and one large.
-%{
-% running sums of errors
-rs_max_eig_error = 0.0;
-rs_max_decomp_error = 0.0;
-rs_time = 0.0;
-diary('multipole.log');
 v = [linspace(0, 0.0000499, 500); linspace(1000, 1000.0000499, 500)]';
 v = v(:);
-for i=1:200
-    i
-    [U, ~] = qr(randn(1000));
-    A = U * diag(v) * U'; % eigenvalues 1:1:100, each with multiplicity 2
-    A = triu(A) + tril(A) - diag(diag(A)); % make it perfectly symmetric
-    A = hess(A); % reduce A to tridiagonal using orthogonal transformations
-    D = diag(A);
-    E = diag(A, 1);
-    tic;
-    [d, q] = dlaed0(D, E);
-    t = toc;
-    max_eig_error = max(abs(d - v));
-    max_decomp_error = max(max(abs(A - q * diag(d) * q')));
-    rs_max_eig_error = rs_max_eig_error + max_eig_error;
-    rs_max_decomp_error = rs_max_decomp_error + max_decomp_error;
-    rs_time = rs_time + t;
-    fprintf(1, 'Running average of max_eig_error = %.12g\n', ...
-        rs_max_eig_error / i);
-    fprintf(1, 'Running average of max_decomp_error = %.12g\n', ...
-        rs_max_decomp_error / i);
-    fprintf(1, 'Running average of running time = %.12g\n', rs_time / i);
-end
-diary off;
-%}
+%random_tester('multipole.log', v, 200);
 
 %% symmetric tridiagonal matrices with (uniformly) random diagonal entries
 % running sums of errors
@@ -121,6 +33,10 @@ for i=1:200
     t = toc;
     max_eig_error = max(abs(d - sort(eig(A))));
     max_decomp_error = max(max(abs(A - q * diag(d) * q')));
+    if isnan(max_decomp_error)
+        save('A.mat', 'A');
+        error('too much error.');
+    end
     rs_max_eig_error = rs_max_eig_error + max_eig_error;
     rs_max_decomp_error = rs_max_decomp_error + max_decomp_error;
     rs_time = rs_time + t;
