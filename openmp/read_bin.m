cd54ea5144425855829b807472822dbdfedacb61
Philip Cho <Hyunsu.Cho@trincoll.edu>
2014-02-22 19:55:21 -0500

    Rearranged files to make the profiler module

--- openmp/read_bin.m
+++
@@ -1,7 +0,0 @@
-function [A] = read_bin(filename)
-
-fid = fopen(filename, 'r');
-M = fread(fid, [1,1], 'integer*8');
-N = fread(fid, [1,1], 'integer*8');
-A = fread(fid, [M,N], 'double');
-fclose(fid);
